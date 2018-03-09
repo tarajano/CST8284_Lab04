32a64c986d3844e2569ec9c56277453921de1027
Manuel A. Alonso Tarajano <tarajano@gmail.com>
2018-03-09 12:31:00 -0500

    Ads initial JUnit test files in new package.

---
+++ test/cst8284/calculator/TestComplex.java
@@ -0,0 +1,14 @@
+package test.cst8284.calculator;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestComplex {
+
+  @Test
+  public void testIsZero() {
+    fail("Not yet implemented");
+  }
+
+}
---
+++ test/cst8284/calculator/TestComplexCalculator.java
@@ -0,0 +1,14 @@
+package test.cst8284.calculator;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestComplexCalculator {
+
+  @Test
+  public void testToString() {
+    fail("Not yet implemented");
+  }
+
+}
